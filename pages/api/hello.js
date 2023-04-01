2677c8058829f90bbdbf3f80b83a11f875f535c5
Jason Liu <111265664+liug26@users.noreply.github.com>
2023-04-01 13:53:05 -0700

    Update Hello API

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { promises as fs } from 'fs';
 
-export default function handler(req, res)
+export default async function handler(req, res)
 {
-
+    const fileContents = await fs.readFile(process.cwd() + '/public/re-static-data.json', 'utf8');
     res.status(200).json({ name: 'John Doe' })
 }
