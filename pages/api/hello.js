b2b2e28f7f73fabb9b32d5267868998ba1b3f7ef
Jason Liu <111265664+liug26@users.noreply.github.com>
2023-04-01 13:49:34 -0700

    Update Hello API

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { promises as fs } from 'fs';
 
-export default function handler(req, res) {
+export default function handler(req, res)
+{
+
     res.status(200).json({ name: 'John Doe' })
-  }
+}
